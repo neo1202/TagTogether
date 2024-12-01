32801e484691a066f2cd7a172441c710dfe952d5
neo1202 <neowuhuayo@gmail.com>
2024-12-01 21:40:19 +0800

    add: proxy routing config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://0.0.0.0:8000", // 後端服務的實際地址
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // 去掉 `/api` 前綴 後端不用care
+      },
+    },
+  },
   plugins: [react()],
-})
+});
