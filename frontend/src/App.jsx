cfa16a369e41ec6e13b706532705fd2d645e80ee
neo1202 <neowuhuayo@gmail.com>
2024-12-01 21:43:15 +0800

    add: main, app setup with outlet context, routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { Outlet, Link, useNavigate, useLocation } from "react-router-dom";
+import Alert from "./pages/status/Alert";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jwtToken, setJwtToken] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertClassName, setAlertClassName] = useState("hidden");
+  const location = useLocation(); // 获取当前路径
+
+  // 用以在切換頁面路徑時清除上個頁面留下來的alert訊息
+  useEffect(() => {
+    setAlertMessage("");
+    setAlertClassName("hidden");
+  }, [location.pathname]); // 每当路径变化时触发
+
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    setJwtToken("");
+    setAlertMessage("You have been logged out.");
+    setAlertClassName("alert-success");
+    navigate("/login");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col min-h-screen bg-slate-100">
+      <header className="flex justify-between items-center p-4 bg-blue-600 text-white">
+        <h1 className="text-xl">JWT Login Test</h1>
+        <nav>
+          {jwtToken ? (
+            <button className="text-red-400" onClick={logOut}>
+              Logout
+            </button>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </nav>
+      </header>
+      <Alert message={alertMessage} className={alertClassName} />
+      <main className="flex-1 p-4">
+        <Outlet
+          context={{
+            jwtToken,
+            setJwtToken,
+            setAlertMessage,
+            setAlertClassName,
+          }}
+        />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import ErrorPage from "./pages/status/Error";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      // {
+      //   path: "/movies/:id",
+      //   element: <Movie />,
+      // },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
