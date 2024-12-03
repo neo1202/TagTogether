cdd1be55937475843b28f1f63676382f0b62e7c5
neo1202 <neowuhuayo@gmail.com>
2024-12-03 23:46:52 +0800

    fix: set default text to black

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media",
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        DEFAULT: "#000000", // 更改預設文字顏色為白色
+        dark: "#000000", // 在夜間模式下將文字顏色設定為黑色
+      },
+    },
   },
   plugins: [],
-}
+};
