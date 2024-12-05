3606ad47f2b664173e7d984a21b4c09c3b64d787
neo1202 <neowuhuayo@gmail.com>
2024-12-06 03:17:23 +0800

    Tiny fix relationship

--- backend/app/models/team_model.py
+++ backend/app/models/team_model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Float, TIMESTAMP
 from sqlalchemy.sql import func
 from app.models.base import Base  # 导入全局 Base
+from sqlalchemy.orm import relationship
 
 class Team(Base):
     __tablename__ = "teams"
@@ -16,3 +17,5 @@ class TeamMember(Base):
     team_name = Column(String)
     user_name = Column(String)
     joined_at = Column(TIMESTAMP, server_default=func.now())
+
+    user = relationship("User", back_populates="team_members")  # team_member.user 可以方便地访问与该记录关联的用户
