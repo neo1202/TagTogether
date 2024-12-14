b8f9f9c317543edb9797e73b0cd2ecc714d46eef
neo1202 <neowuhuayo@gmail.com>
2024-12-14 17:21:31 +0800

    Tiny fix format

--- frontend/src/hooks/useApi.jsx
+++ frontend/src/hooks/useApi.jsx
@@ -1,16 +1,20 @@
 import { useOutletContext } from "react-router-dom";
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import React, { useCallback } from "react";
 
 export const useApi = () => {
   const { jwtToken } = useOutletContext();
 
-  const fetchWithToken = useCallback(async (url, options = {}) => {  // options: 请求的其他选项，例如 method 和 body，默认为空对象 {}
-    const headers = {
-      ...options.headers,  // 合并原有的请求头（options.headers）和新的 Authorization 头部
-      Authorization: `Bearer ${jwtToken}`,
-    };
-    return fetch(url, { ...options, headers });
-  }, [jwtToken]);
+  const fetchWithToken = useCallback(
+    async (url, options = {}) => {
+      // options: 请求的其他选项，例如 method 和 body，默认为空对象 {}
+      const headers = {
+        ...options.headers, // 合并原有的请求头（options.headers）和新的 Authorization 头部
+        Authorization: `Bearer ${jwtToken}`,
+      };
+      return fetch(url, { ...options, headers });
+    },
+    [jwtToken]
+  );
 
   return { fetchWithToken };
 };
