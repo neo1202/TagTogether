03bc2540fb5803c661dd86d4a8edc8365b9eeb2a
Alison <75669495+Cyalisonliu@users.noreply.github.com>
2024-12-17 12:13:34 +0800

    feat: add dotenv for environment variable management and update backend URL configuration in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://0.0.0.0:8000", // 後端服務的實際地址
+        target: process.env.BACKEND_URL, // 後端服務的實際地址
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""), // 去掉 `/api` 前綴 後端不用care
       },
